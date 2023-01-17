027a23f64954c6fcbdb5f69d7031cfe3f8e7ac42
huahua2 <lghua2009@163.com>
2023-01-18 00:27:58 +0800

    feat: 操作手柄绘制

--- js/content-script.js
+++ js/content-script.js
@@ -7,7 +7,7 @@ let sCss="";
 
 for (var i=0;i<360;i++) {
 
-  sCss+=".helloworld li:nth-of-type("+(i+1)+"){-webkit-transform: rotate("+i*1+"deg);}";
+  sCss+=".helloworld li:nth-of-type("+(i)+"){-webkit-transform: rotate("+i*1+"deg);}";
 
   if ((i + 1) % 10 === 0) {
     oLi+="<li><em>"+(i+1)+"</em></li>";
@@ -18,7 +18,7 @@ for (var i=0;i<360;i++) {
 
 };
 
-helloWorld.innerHTML= "<ul>" + oLi + '</ul>';
+helloWorld.innerHTML= "<ul>" + oLi + '</ul><div class="line_wrap"><div class="container"><div class="line_0"></div><div class="line_180"></div><div class="line_90"></div><div class="line_270"></div></div></div>';
 
 style.innerHTML+=sCss;
 helloWorld.className = 'helloworld'
