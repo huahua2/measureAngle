8aac777093e9843cf28a7fa46f75ac3412a3080d
liuguanghua <anotherhua@gmail.com>
2023-01-19 14:50:36 +0800

    perf: 90°在上方

--- js/content-script.js
+++ js/content-script.js
@@ -11,6 +11,11 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   }
 })
 
+
+function radian2Angle(radian) {
+  return radian * -180 / Math.PI
+}
+
 const calcStateRotation = (rotation) => {
   rotation = rotation % 360
 
@@ -20,7 +25,7 @@ const calcStateRotation = (rotation) => {
 
   return Number(rotation).toFixed(2)
 }
-
+// 对角
 const calOppositeAngle = (curDeg) => {
   let a = Number(curDeg) + 180
   if (a > 360) {
@@ -29,32 +34,37 @@ const calOppositeAngle = (curDeg) => {
   return Number(a).toFixed(2)
 }
 
+const calCssAngle = (angle, decimal = 2) => {
+  const calDeg = 90 - angle
+  return Number(calcStateRotation(calDeg)).toFixed(decimal)
+}
 const clear = () => {
   document.onmousemove = null
   document.onmouseup = null
 }
 
-function  create() {
+function create() {
   const measureAngle = document.createElement('div');
   const style = document.createElement('style');
 
-  let oLi="";
-  let sCss="";
+  let oLi = "";
+  let sCss = "";
 
-  for (let i=0; i<360; i++) {
+  for (let i = 0; i < 360; i++) {
 
     sCss += ".measure-angle li:nth-of-type(" + (i) + "){-webkit-transform: rotate(" + i * 1 + "deg);}";
 
     if ((i + 1) % 10 === 0) {
-      oLi += "<li><em>" + (i + 1 === 360 ? 0 : i + 1) + "</em></li>";
+      oLi += "<li><em>" + calCssAngle(i + 1, 0) + "</em></li>";
     } else {
       oLi += "<li></li>";
     }
-  };
+  }
+  ;
 
-  measureAngle.innerHTML= '<div class="line_wrap"><div class="container">' +"<ul>" + oLi + '</ul><div class="line_0"></div><div class="line_180"></div><div class="line_90">90</div><div class="line_270">270</div></div></div>';
+  measureAngle.innerHTML = '<div class="inner_circle"></div><div class="line_wrap"><div class="container">' + "<ul>" + oLi + '</ul><div class="line_0"></div><div class="line_180"></div><div class="line_90">90</div><div class="line_270">270</div></div></div>';
 
-  style.innerHTML+=sCss;
+  style.innerHTML += sCss;
   measureAngle.className = 'measure-angle'
 
   document.head.appendChild(style);
@@ -62,20 +72,7 @@ function  create() {
 
 }
 
-function changeRadius () {
-  const exist = document.getElementById('changeRadius')
-  if (exist) {
-    exist.remove()
-  }
-  const style = document.createElement('style');
-  style.id = 'changeRadius'
-  const sCss = `.measure-angle {display: block; width: ${radius * 2 - 80}px; height: ${radius * 2 - 80}px;}.measure-angle li { transform-origin: center ${radius}px; left: ${radius - 1}px } .line_0,.line_90,.line_180,.line_270 {
-  transform-origin: center ${radius}px; left: ${radius - 1}px}`
-  style.innerHTML+=sCss;
-  document.head.appendChild(style);
-}
-
-function bindOplineEvent () {
+function bindOplineEvent() {
 
   let _dragEle = document.querySelector('.measure-angle')
   let _line90 = document.querySelector('.line_90')
@@ -91,7 +88,7 @@ function bindOplineEvent () {
       // 按下位置
 
       const bound = _dragEle.getBoundingClientRect()
-      const center = {x: bound.left + bound.width / 2, y: bound.top + bound.height / 2 }
+      const center = {x: bound.left + bound.width / 2, y: bound.top + bound.height / 2}
       document.onmousemove = (e) => {
         if (!_act) {
           return
@@ -99,18 +96,18 @@ function bindOplineEvent () {
         const downX = e.clientX
         const downY = e.clientY
         const radian = Math.atan2(downY - center.y, downX - center.x)
-        const deg = radian * 180 / Math.PI + 90
+        const deg = radian2Angle(radian)
         const curDeg = calcStateRotation(deg)
-        // console.log('度数', curDeg)
-        _line90.style.transform = `rotate(${ curDeg }deg)`;
+        const cssAngle = calCssAngle(curDeg)
+        _line90.style.transform = `rotate(${cssAngle}deg)`;
         _line90.style.height = `60px`;
         _line90.innerHTML = curDeg
 
 
-        const oppositeAngle = calOppositeAngle(curDeg)
-        _line270.style.transform = `rotate(${ oppositeAngle }deg)`;
+        const oppositeAngle = calOppositeAngle(cssAngle)
+        _line270.style.transform = `rotate(${oppositeAngle}deg)`;
         _line270.style.height = `60px`;
-        _line270.innerHTML = oppositeAngle
+        _line270.innerHTML = calcStateRotation(curDeg - 180)
       }
       document.onmouseup = (e) => {
         _line270.style.removeProperty('height')
@@ -133,10 +130,7 @@ function bindOplineEvent () {
       // 按下位置
 
       const bound = _dragEle.getBoundingClientRect()
-      const center = {x: bound.left + bound.width / 2, y: bound.top + bound.height / 2 }
-      console.log('center', center)
-
-
+      const center = {x: bound.left + bound.width / 2, y: bound.top + bound.height / 2}
       document.onmousemove = (e) => {
         if (!_act) {
           return
@@ -144,16 +138,16 @@ function bindOplineEvent () {
         const downX = e.clientX
         const downY = e.clientY
         const radian = Math.atan2(downY - center.y, downX - center.x)
-        const deg = radian * 180 / Math.PI + 90
+        const deg = radian2Angle(radian)
         const curDeg = calcStateRotation(deg)
-        // console.log('度数', curDeg.toFixed(2))
-        _line270.style.transform = `rotate(${ curDeg }deg)`;
+        const cssAngle = calCssAngle(curDeg)
+        _line270.style.transform = `rotate(${cssAngle}deg)`;
         _line270.innerHTML = curDeg
         _line270.style.height = `60px`;
 
-        const oppositeAngle = calOppositeAngle(curDeg)
-        _line90.style.transform = `rotate(${ oppositeAngle }deg)`;
-        _line90.innerHTML = oppositeAngle
+        const oppositeAngle = calOppositeAngle(cssAngle)
+        _line90.style.transform = `rotate(${oppositeAngle}deg)`;
+        _line90.innerHTML = calcStateRotation(curDeg - 180)
         _line90.style.height = `60px`;
       }
       document.onmouseup = (e) => {
@@ -164,10 +158,9 @@ function bindOplineEvent () {
       }
     })
   }
-
 }
 
-function bindMeasureAngleEvent () {
+function bindMeasureAngleEvent() {
   let _dragEle = document.querySelector('.measure-angle')
   let _act = false
 
@@ -191,7 +184,7 @@ function bindMeasureAngleEvent () {
         const left = e.clientX - downX + bcr.x
         const top = e.clientY - downY + bcr.y
         const bound = _dragEle.getBoundingClientRect()
-        const { width, height } = bound
+        const {width, height} = bound
         _dragEle.style.left = Math.min(Math.max(left, -(width / 2)), window.innerWidth - width + 10) + 'px'
         _dragEle.style.top = Math.min(Math.max(top, 0), window.innerHeight - height / 5) + 'px'
         _dragEle.style.transform = 'none'
@@ -207,10 +200,26 @@ function bindMeasureAngleEvent () {
 
 
   if (_dragEle) {
-  _dragEle.addEventListener('mousedown', dragEnter)
+    _dragEle.addEventListener('mousedown', dragEnter)
   }
 
 }
+
+function changeRadius() {
+  const exist = document.getElementById('changeRadius')
+  if (exist) {
+    exist.remove()
+  }
+  const style = document.createElement('style');
+  style.id = 'changeRadius'
+  // 92是边框的宽度
+  const BORDER_WIDTH = 92
+  const WIDTH = radius * 2
+  const sCss = `.measure-angle {display: block; width: ${WIDTH - BORDER_WIDTH}px; height: ${WIDTH - BORDER_WIDTH}px;}.measure-angle li { transform-origin: center ${radius}px; left: ${radius - 1}px; } .line_0,.line_90,.line_180,.line_270 { transform-origin: center ${radius}px; left: ${radius - 1}px; }`
+  style.innerHTML += sCss;
+  document.head.appendChild(style);
+}
+
 
 create()
 bindOplineEvent()
